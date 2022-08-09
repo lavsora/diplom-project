f92f6745198c895308d22ac4b33e74c16ab4fedd
lavsora <ya.karpich@gmail.com>
2022-08-09 14:09:04 +0300

    servicesModal module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import headerModal from './modules/headerModal'
 import timer from './modules/timer'
 import scroll from './modules/scroll'
+import servicesModal from './modules/servicesModal'
 
 headerModal()
 timer('13 august 2022')
 scroll()
+servicesModal()
