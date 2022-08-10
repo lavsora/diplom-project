6d8d57f3ecb2f024ab1b81f8a386aef8e19ceadf
lavsora <ya.karpich@gmail.com>
2022-08-10 12:04:26 +0300

    validation module add

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import headerModal from './modules/headerModal'
 import timer from './modules/timer'
 import scroll from './modules/scroll'
 import servicesModal from './modules/servicesModal'
+import validation from './modules/validation'
 
 headerModal()
 timer('13 august 2022')
 scroll()
 servicesModal()
+validation()
