f32a70b86ea52ee61a2eb6311a21f2d779051a3e
lavsora <ya.karpich@gmail.com>
2022-08-11 08:01:36 +0300

    calculator module add

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,31 +1,35 @@
 const calculator = () => {
-    const calcBlock = document.querySelector('#calc')
-    const calcType = calcBlock.querySelector('#calc-type')
-    const calcTypeMaterial = calcBlock.querySelector('#calc-type-material')
-    const calcInput = calcBlock.querySelector('#calc-input')
-    const calcTotal = calcBlock.querySelector('#calc-total')
+    try {
+        const calcBlock = document.querySelector('#calc')
+        const calcType = calcBlock.querySelector('#calc-type')
+        const calcTypeMaterial = calcBlock.querySelector('#calc-type-material')
+        const calcInput = calcBlock.querySelector('#calc-input')
+        const calcTotal = calcBlock.querySelector('#calc-total')
 
-    const countCalc = () => {
-        const calcTypeValue = +calcType.value
-        const calcTypeMaterialValue = +calcTypeMaterial.value
+        const countCalc = () => {
+            const calcTypeValue = +calcType.value
+            const calcTypeMaterialValue = +calcTypeMaterial.value
 
-        let totalValue = 0
+            let totalValue = 0
 
-        if (calcTypeValue && calcTypeMaterialValue) {
-            totalValue = calcTypeValue * calcTypeMaterialValue * calcInput.value
-        } else {
-            totalValue = 0
+            if (calcTypeValue && calcTypeMaterialValue) {
+                totalValue = calcTypeValue * calcTypeMaterialValue * calcInput.value
+            } else {
+                totalValue = 0
+            }
+
+            calcTotal.value = totalValue
         }
 
-        calcTotal.value = totalValue
+        calcBlock.addEventListener('input', (e) => {
+            if (e.target === calcType || e.target === calcTypeMaterial ||
+                e.target === calcInput) {
+                countCalc()
+            }
+        })
+    } catch (error) {
+        console.log(error.message)
     }
-
-    calcBlock.addEventListener('input', (e) => {
-        if (e.target === calcType || e.target === calcTypeMaterial ||
-            e.target === calcInput) {
-                countCalc() 
-        }
-    })
 
 }
 
